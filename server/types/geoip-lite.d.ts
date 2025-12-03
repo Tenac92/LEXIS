3ae3cbe6c67dad8133b4ea3055f041bdca4a6fba
Tenac92 <kkyriakou1992@gmail.com>
2025-12-03 12:09:49 +0000

    geoip

--- server/types/geoip-lite.d.ts
+++ server/types/geoip-lite.d.ts
@@ -1,29 +1,29 @@
-declare module 'geoip-lite' {
-  interface GeoIpLookupInfo {
-    range: [number, number];
-    country: string;
-    region: string;
-    eu: '1' | '0';
-    timezone: string;
-    city: string;
-    ll: [number, number]; // latitude, longitude
-    metro: number;
-    area: number;
-  }
+// declare module 'geoip-lite' {
+//   interface GeoIpLookupInfo {
+//     range: [number, number];
+//     country: string;
+//     region: string;
+//     eu: '1' | '0';
+//     timezone: string;
+//     city: string;
+//     ll: [number, number]; // latitude, longitude
+//     metro: number;
+//     area: number;
+//   }
 
-  function lookup(ip: string): GeoIpLookupInfo | null;
-  function pretty(ip: string): string;
-  function startWatchingDataUpdate(): void;
-  function stopWatchingDataUpdate(): void;
-  function reloadDataSync(): void;
-  function reload(callback: (err: Error) => void): void;
+//   function lookup(ip: string): GeoIpLookupInfo | null;
+//   function pretty(ip: string): string;
+//   function startWatchingDataUpdate(): void;
+//   function stopWatchingDataUpdate(): void;
+//   function reloadDataSync(): void;
+//   function reload(callback: (err: Error) => void): void;
 
-  export = {
-    lookup,
-    pretty,
-    startWatchingDataUpdate,
-    stopWatchingDataUpdate,
-    reloadDataSync,
-    reload
-  };
-}
+//   export = {
+//     lookup,
+//     pretty,
+//     startWatchingDataUpdate,
+//     stopWatchingDataUpdate,
+//     reloadDataSync,
+//     reload
+//   };
+// }
